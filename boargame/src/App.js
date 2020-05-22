0f872b3400fdda05f2b1ace5bfcb9e3c546ade1c
Rudson Varona <rjvarona16@gmail.com>
2020-05-22 14:15:46 -0700

    fixing black and whtie style issues

--- boargame/src/App.js
+++ boargame/src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 function App() {
   return (
+
+
     <div className="App">
-
-
       <Board />
     </div>
   );
@@ -17,39 +17,84 @@ export default App;
 debugger
 
 
-//input
+//squares
 const Square = (props) => {
-  if (props.index % 2 === 0) {
+  if (props.index % 2 == 0) {
     return (
       <button style={{ width: "100px", height: "100px", backgroundColor: "black" }}>{props.value} </button>
     );
   }
   else {
     return (
-      <button style={{ width: "100px", height: "100px" }}>{props.value} </button>
+      <button style={{ width: "100px", height: "100px"}}>{props.value} </button>
     )
   }
 }
 
-const Board = (index) => {
+
+
+
+const Board = (props) => {
 
   //copy board
   const [board, setBoard] = useState(Array(9).fill(null));
 
 
-  //render the board
-  const render = (index) => {
-    return (
-      <Square index={index} value={board[index]}></Square>
-    );
+  //boardValue
+  const [boardValue, setboardValue] = useState(3);
+
+
+  //render squares
+  const renderSquare = () => {
+    let children = []
+    for (let i = 0; i < (boardValue); i++) {
+
+        children.push(
+          <Square index={i} value={board[i]}></Square>
+        );
+    }
+
+    return children;
   }
+
+
+
+  //render the board
+  const render = () => {
+    let children = []
+    for (let i = 0; i < (boardValue); i++) {
+      
+     
+      children.push(
+        <div>{renderSquare(true)}</div>
+      );
+    }
+
+    return children;
+  }
+
+  //update Board
+  const onChangeValue = (e) => {
+    setboardValue(e.target.value);
+  }
+
+  const updateBoard = () => {
+    setBoard(Array(boardValue).fill(boardValue))
+    render()
+  }
+
   return (
-
+    //
+    // <a href="#" onClick={() => { props.update(props.value) }}>Update Squares Board </a>
     <div>
+      <input value={boardValue} onChange={onChangeValue.bind(this)}>
+      </input>
 
-      <div>{render(0)}{render(1)}{render(2)}</div>
-      <div>{render(3)}{render(4)}{render(5)}</div>
-      <div>{render(6)}{render(7)}{render(8)}</div>
+
+      {/* update the board */}
+      <button onClick={updateBoard}>change</button>
+
+      {render()}
     </div>
   );
 
