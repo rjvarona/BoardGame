732fb03a8abcd7b9b6c322a0f535a9b16379bee5
Rudson Varona <rjvarona16@gmail.com>
2020-05-22 12:56:14 -0700

    no message

--- boargame/src/App.js
+++ boargame/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Square/>
     </div>
   );
 }
 
 export default App;
+
+debugger
+//
+const Square = (props) => {
+    return(
+      <button style={{width: "100px", height:"100px" }} onClick={props.onClick}>{props.value} </button>
+    );
+}
