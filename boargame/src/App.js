ac940725b7f17a1f1d8bedcc4385f0980287b8b1
Rudson Varona <rjvarona16@gmail.com>
2020-05-22 13:05:23 -0700

    added color to board

--- boargame/src/App.js
+++ boargame/src/App.js
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Square/>
+
+
+      <Board />
     </div>
   );
 }
@@ -13,9 +15,42 @@ function App() {
 export default App;
 
 debugger
-//
+
+
+//input
 const Square = (props) => {
-    return(
-      <button style={{width: "100px", height:"100px" }} onClick={props.onClick}>{props.value} </button>
+  if (props.index % 2 === 0) {
+    return (
+      <button style={{ width: "100px", height: "100px", backgroundColor: "black" }}>{props.value} </button>
     );
+  }
+  else {
+    return (
+      <button style={{ width: "100px", height: "100px" }}>{props.value} </button>
+    )
+  }
+}
+
+const Board = (index) => {
+
+  //copy board
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+
+  //render the board
+  const render = (index) => {
+    return (
+      <Square index={index} value={board[index]}></Square>
+    );
+  }
+  return (
+
+    <div>
+
+      <div>{render(0)}{render(1)}{render(2)}</div>
+      <div>{render(3)}{render(4)}{render(5)}</div>
+      <div>{render(6)}{render(7)}{render(8)}</div>
+    </div>
+  );
+
 }
